04e37c7caa1374edb8a167d4844dae5e80540759
conashin <conashinc@gmail.com>
2024-07-15 02:57:10 +0800

    Update build.sh

--- module/build.sh
+++ module/build.sh
@@ -3,4 +3,4 @@
 pyinstaller --onefile sav_cli.py -n sav_cli_$(uname -s | tr 'A-Z' 'a-z')_$(uname -m | tr 'A-Z' 'a-z')
 
 # Compile for aarch64
-pyinstaller --onefile sav_cli.py -n sav_cli_linux_aarch64 --distpath dist/aarch64 --workpath build/aarch64 --specpath spec/aarch64 --additional-hooks-dir=hooks
+pyinstaller --onefile sav_cli.py -n sav_cli_linux_aarch64
